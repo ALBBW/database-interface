bdeb29772e5e92103edbb34597d420edcdf90207
GamesGamble <gamesgamble@posteo.de>
2018-11-06 14:12:08 +0100

    Creating the first structure of the Project.

---
+++ src/chiper/dbinterface/Controler.java
@@ -0,0 +1,24 @@
+/**
+ * Using the dbinterface package of the chiper program.
+ */
+package chiper.dbinterface;
+
+/**
+ * This is the Controler class, that is based on the MVC design pattern and controls everything.
+ * @author Joscha Alhelm
+ */
+public class Controler 
+{
+
+	/**
+	 * This is the main method, that is only used to start the start method of the module.
+	 * @param args start parameter of the program
+	 */
+	public static void main(String[] args) 
+	{
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/chiper/dbinterface/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This Package is the Database Interface module from the program Chiper.
+ * This module should only interact with the connector module, that connects this Database Interface to the other 
+ *  modules of the Chiper program.
+ *  
+ *  @author Joscha Alhelm
+ */
+package chiper.dbinterface;
