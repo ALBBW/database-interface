15a70412be5f492f98b86bad9fc2b9a9809f9bb9
GamesGamble <gamesgamble@posteo.de>
2019-02-20 10:38:08 +0100

    Revert "Updated Unfinished Code"
    This reverts commit e974a738687875b68874fdba1b80dfbba677c712.

---
+++ src/chiper/dbinterface/Model_Output_MariaDB.java
@@ -0,0 +1,23 @@
+/**
+ * filename: 					Model_Output_MariaDB.java
+ * @author 						GamesGamble
+ * creation date: 		20.11.2018
+ * alteration date:		26.11.2018
+ */
+
+/**
+ * Using the dbinterface package of the chiper program.
+ */
+package chiper.dbinterface;
+
+public class Model_Output_MariaDB implements Model_Output 
+{
+
+	@Override
+	public void setData(String data) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/de/albbw/chiper/dbinterface/constructor/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * filename: package-info.java
- * 
- * @author GamesGamble creation date: 05.02.2019 alteration date: 05.02.2019
- */
-/**
- * TODO: Create Comment for package-info.
- */
-package de.albbw.chiper.dbinterface.constructor;
